9f5befe8a91525853d74136e171bd5be76a12438
Gleison Brito <brittobaptista93@gmail.com>
2023-10-23 11:52:47 -0300

    Update script_criar_banco.sql

--- Banco_De_Dados/TP02/script_criar_banco.sql
+++ Banco_De_Dados/TP02/script_criar_banco.sql
@@ -1,5 +1,7 @@
+drop database bdestoqueloja;
+
 -- Criação do Banco de Dados
-CREATE TABLE bdEstoqueLoja;
+CREATE database bdEstoqueLoja;
 USE bdEstoqueLoja;
 
 -- Criação das Tabelas
@@ -55,3 +57,62 @@ CREATE TABLE ItensDeVenda (
     FOREIGN KEY (IDDaVenda) REFERENCES Vendas(ID)        -- Relação com a venda.
 );
 
+insert into CategoriasDeProdutos (ID, NomeDaCategoria)
+values (1, "Farinha");
+
+insert into CategoriasDeProdutos (ID, NomeDaCategoria)
+values (2, "Grao"),
+(3, "Utensilio"),
+(4, "Cereal");
+
+insert into fornecedores (ID, Nome, Endereco, Telefone)
+	values (1, "Tambasa", "Rua Pedro, nº75 - Centro","31-3335-5555");
+
+insert into fornecedores (ID, Nome, Endereco, Telefone)
+	values (2, "Ceasa", "BR 040, nº2000 - Ribeirao das Neves","31-3451-7878"),
+(3, "Vilma", "Rua 17, nº854 - Belo Horizonte","31-3574-1524");
+
+
+insert into Produtos (ID, nome, descricao, preco, QuantidadeEmEstoque, categoria, Fornecedor)
+	values (1, "Batata palha", "", "5.50", "10", 3, 3),
+	(2, "Toddynho", "", "5", "15", 3, 2);
+
+insert into Vendas (ID, DataDaVenda, ValorTotal, Funcionario)
+	values (1, "2023-10-23", "300.00", "fulano de tal"),
+    (2, "2023-10-14", "150.00", "cicrano de tal");
+
+insert into ItensDeVenda(ID, IDDoProduto, IDDaVenda, QuantidadeVendida, PrecoUnitario)
+	values (1, 1, 1, 30, "10.00"),
+    (2, 2, 1, 28, "5.00"),
+    (3, 2, 2, 50, "25.00");
+    
+-- questao 01
+select * from Produtos;
+
+-- questao 02
+select nome from Fornecedores;
+
+-- questao 03
+select * from CategoriasDeProdutos;
+
+-- questao 04
+select * from Produtos where Fornecedor = 2;
+
+-- questao 05
+select * from Produtos where Preco > 5;
+
+
+-- questao 06
+-- step 01
+select * from Vendas inner join ItensDeVenda; -- cruza todas as linhas de Vendas com ItensDeVenda
+
+-- step 02
+select * from Vendas V inner join ItensDeVenda IV; -- coloca apelidos nas tabelas para evitar ambiguidade (ItensDeVenda e Vendas tem o mesmo campo ID) 
+
+-- step 03
+select * from Vendas V inner join ItensDeVenda IV 
+on V.ID = IV.IDDaVenda; -- seleciona a intersecção das duas tabelas (onde o ID da Venda é igual o IDDaVenda de ItensDeVenda)
+
+-- step 04
+select * from Vendas V inner join ItensDeVenda IV 
+on V.ID = IV.IDDaVenda where IV.IDDoProduto = 2; -- filtra pelo ID do produto
