96f890f3239bbe17736f2230df9e77ae1bc51aa8
Gleison Brito <brittobaptista93@gmail.com>
2023-10-24 23:33:30 -0300

    update popular banco

--- Banco_De_Dados/TP02/script_popular_banco.sql
+++ Banco_De_Dados/TP02/script_popular_banco.sql
@@ -10,18 +10,18 @@ VALUES
 (8, "Higiene"),
 (9, "Hortifruti");
 
-INSERT INTO Fornecedores (ID, Nome, Endereco, Cidade, Telefone)
+INSERT INTO Fornecedores (ID, Nome, Endereco, Telefone)
 VALUES
-    (1, 'Fornecedor 1', 'Rua A, 123', 'São Paulo', '(11) 1234-5678'),
-    (2, 'Fornecedor 2', 'Avenida B, 456', 'Rio de Janeiro', '(21) 2345-6789'),
-    (3, 'Fornecedor 3', 'Travessa C, 789', 'Belo Horizonte', '(31) 3456-7890'),
-    (4, 'Fornecedor 4', 'Rua D, 234', 'Porto Alegre', '(51) 4567-8901'),
-    (5, 'Fornecedor 5', 'Avenida E, 567', 'Salvador', '(71) 5678-9012'),
-    (6, 'Fornecedor 6', 'Travessa F, 890', 'Brasília', '(61) 6789-0123'),
-    (7, 'Fornecedor 7', 'Rua G, 123', 'Curitiba', '(41) 7890-1234'),
-    (8, 'Fornecedor 8', 'Avenida H, 234', 'Fortaleza', '(85) 8901-2345'),
-    (9, 'Fornecedor 9', 'Travessa I, 567', 'Recife', '(81) 9012-3456'),
-    (10, 'Fornecedor 10', 'Rua J, 890', 'Manaus', '(92) 0123-4567');
+    (1, 'Fornecedor 1', 'Rua A, 123 São Paulo', '(11) 1234-5678'),
+    (2, 'Fornecedor 2', 'Avenida B, 456 Rio de Janeiro', '(21) 2345-6789'),
+    (3, 'Fornecedor 3', 'Travessa C, 789 Belo Horizonte', '(31) 3456-7890'),
+    (4, 'Fornecedor 4', 'Rua D, 234 Porto Alegre', '(51) 4567-8901'),
+    (5, 'Fornecedor 5', 'Avenida E, 567 Salvador', '(71) 5678-9012'),
+    (6, 'Fornecedor 6', 'Travessa F, 890 Brasília', '(61) 6789-0123'),
+    (7, 'Fornecedor 7', 'Rua G, 123 Curitiba', '(41) 7890-1234'),
+    (8, 'Fornecedor 8', 'Avenida H, 234 Fortaleza', '(85) 8901-2345'),
+    (9, 'Fornecedor 9', 'Travessa I, 567 Recife', '(81) 9012-3456'),
+    (10, 'Fornecedor 10', 'Rua J, 890 Manaus', '(92) 0123-4567');
 
 INSERT INTO Produtos (ID, Nome, Descricao, Preco, QuantidadeEmEstoque, Categoria, Fornecedor)
 VALUES
@@ -95,47 +95,47 @@ VALUES
     (57, 'Vinho Tinto', 'Garrafa de vinho tinto', 12.99, 30, 6, 8),
     (58, 'Água de Coco', 'Água de coco natural', 2.29, 40, 6, 9),
     (59, 'Refrigerante de Limão', 'Sprite 2L', 2.29, 60, 6, 1),
-    (60, 'Energético', 'Lata de energético', 3.49, 25, 6, 4);
+    (60, 'Energético', 'Lata de energético', 3.49, 25, 6, 4),
 
     -- Produtos de Limpeza (Categoria 7)
-    (51, 'Detergente', 'Detergente de limão', 1.29, 150, 7, 9),
-    (52, 'Sabão em Pó', 'Sabão em pó para roupas', 5.99, 80, 7, 8),
-    (53, 'Desinfetante', 'Desinfetante de lavanda', 2.49, 100, 7, 10),
-    (54, 'Esponjas', 'Pacote de esponjas', 1.99, 120, 7, 7),
-    (55, 'Papel Toalha', 'Rolo de papel toalha', 2.79, 70, 7, 2),
-    (56, 'Limpador Multiuso', 'Limpador multiuso', 3.49, 60, 7, 4),
-    (57, 'Amaciante de Roupa', 'Amaciante floral', 4.99, 50, 7, 5),
-    (58, 'Sacos de Lixo', 'Pacote de sacos de lixo', 3.29, 80, 7, 3),
-    (59, 'Alvejante', 'Alvejante sem cloro', 2.59, 70, 7, 1),
-    (60, 'Mop', 'Mop de limpeza', 9.99, 40, 7, 6),
+    (61, 'Detergente', 'Detergente de limão', 1.29, 150, 7, 9),
+    (62, 'Sabão em Pó', 'Sabão em pó para roupas', 5.99, 80, 7, 8),
+    (63, 'Desinfetante', 'Desinfetante de lavanda', 2.49, 100, 7, 10),
+    (64, 'Esponjas', 'Pacote de esponjas', 1.99, 120, 7, 7),
+    (65, 'Papel Toalha', 'Rolo de papel toalha', 2.79, 70, 7, 2),
+    (66, 'Limpador Multiuso', 'Limpador multiuso', 3.49, 60, 7, 4),
+    (67, 'Amaciante de Roupa', 'Amaciante floral', 4.99, 50, 7, 5),
+    (68, 'Sacos de Lixo', 'Pacote de sacos de lixo', 3.29, 80, 7, 3),
+    (69, 'Alvejante', 'Alvejante sem cloro', 2.59, 70, 7, 1),
+    (70, 'Mop', 'Mop de limpeza', 9.99, 40, 7, 6),
 
     -- Produtos de Higiene (Categoria 8)
-    (61, 'Sabonete', 'Sabonete em barra', 0.99, 300, 8, 6),
-    (62, 'Shampoo', 'Shampoo hidratante', 4.49, 150, 8, 2),
-    (63, 'Escova de Dentes', 'Escova de dentes macia', 2.49, 200, 8, 4),
-    (64, 'Creme Dental', 'Creme dental branqueador', 2.99, 180, 8, 7),
-    (65, 'Papel Higiênico', 'Rolo de papel higiênico', 1.79, 220, 8, 5),
-    (66, 'Lenços Umedecidos', 'Lenços umedecidos', 3.99, 120, 8, 1),
-    (67, 'Fio Dental', 'Fio dental com sabor', 1.29, 160, 8, 3),
-    (68, 'Desodorante', 'Desodorante roll-on', 2.99, 140, 8, 10),
-    (69, 'Sabonete Líquido', 'Sabonete líquido de lavanda', 3.49, 110, 8, 8),
-    (70, 'Absorvente Feminino', 'Absorvente com abas', 4.99, 100, 8, 9),
+    (71, 'Sabonete', 'Sabonete em barra', 0.99, 300, 8, 6),
+    (72, 'Shampoo', 'Shampoo hidratante', 4.49, 150, 8, 2),
+    (73, 'Escova de Dentes', 'Escova de dentes macia', 2.49, 200, 8, 4),
+    (74, 'Creme Dental', 'Creme dental branqueador', 2.99, 180, 8, 7),
+    (75, 'Papel Higiênico', 'Rolo de papel higiênico', 1.79, 220, 8, 5),
+    (76, 'Lenços Umedecidos', 'Lenços umedecidos', 3.99, 120, 8, 1),
+    (77, 'Fio Dental', 'Fio dental com sabor', 1.29, 160, 8, 3),
+    (78, 'Desodorante', 'Desodorante roll-on', 2.99, 140, 8, 10),
+    (79, 'Sabonete Líquido', 'Sabonete líquido de lavanda', 3.49, 110, 8, 8),
+    (80, 'Absorvente Feminino', 'Absorvente com abas', 4.99, 100, 8, 9),
 
     -- Produtos de Hortifruti (Categoria 9)
-    (71, 'Maçãs', 'Maçãs vermelhas frescas', 0.79, 80, 9, 3),
-    (72, 'Bananas', 'Bananas maduras', 0.59, 100, 9, 9),
-    (73, 'Cenouras', 'Cenouras orgânicas', 1.29, 70, 9, 7),
-    (74, 'Alface', 'Alface americana', 1.99, 60, 9, 4),
-    (75, 'Tomates', 'Tomates orgânicos', 2.49, 50, 9, 5),
-    (76, 'Brócolis', 'Brócolis frescos', 1.79, 40, 9, 6),
-    (77, 'Morangos', 'Morangos frescos', 2.99, 30, 9, 1),
-    (78, 'Laranjas', 'Laranjas suculentas', 1.49, 70, 9, 8),
-    (79, 'Uvas', 'Uvas sem sementes', 3.99, 40, 9, 2),
-    (80, 'Abacaxi', 'Abacaxi maduro', 2.99, 30, 9, 10);
+    (81, 'Maçãs', 'Maçãs vermelhas frescas', 0.79, 80, 9, 3),
+    (82, 'Bananas', 'Bananas maduras', 0.59, 100, 9, 9),
+    (83, 'Cenouras', 'Cenouras orgânicas', 1.29, 70, 9, 7),
+    (84, 'Alface', 'Alface americana', 1.99, 60, 9, 4),
+    (85, 'Tomates', 'Tomates orgânicos', 2.49, 50, 9, 5),
+    (86, 'Brócolis', 'Brócolis frescos', 1.79, 40, 9, 6),
+    (87, 'Morangos', 'Morangos frescos', 2.99, 30, 9, 1),
+    (88, 'Laranjas', 'Laranjas suculentas', 1.49, 70, 9, 8),
+    (89, 'Uvas', 'Uvas sem sementes', 3.99, 40, 9, 2),
+    (90, 'Abacaxi', 'Abacaxi maduro', 2.99, 30, 9, 10);
 
 INSERT INTO Vendas (ID, DataDaVenda, ValorTotal, Funcionario)
 VALUES
-    (1, '2023-10-01', 24,05, 'João Silva'),
+    (1, '2023-10-01', 24.05, 'João Silva'),
     (2, '2023-10-02', 75.95, 'Maria Santos'),
     (3, '2023-10-03', 31.92, 'Pedro Oliveira'),
     (4, '2023-10-04', 42.82, 'Ana Costa'),
