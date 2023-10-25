fa30a88f953460037879632e03927a2b66841b57
Gleison Brito <brittobaptista93@gmail.com>
2023-10-25 01:38:00 -0300

    some fixes

--- Banco_De_Dados/TP02/script_consultas.sql
+++ Banco_De_Dados/TP02/script_consultas.sql
@@ -28,3 +28,26 @@ on V.ID = IV.IDDaVenda; -- seleciona a intersecção das duas tabelas (onde o ID
 -- step 04
 select * from Vendas V inner join ItensDeVenda IV 
 on V.ID = IV.IDDaVenda where IV.IDDoProduto = 2; -- filtra pelo ID do produto
+
+-- questao 07
+select SUM(QuantidadeEmEstoque * Preco) from Produtos;
+
+-- questao 08
+select Categoria, SUM(QuantidadeEmEstoque * Preco) from Produtos
+group by Categoria;
+
+-- questao 09
+select * from Produtos where Categoria = 2;
+
+-- questao 10
+select * from Vendas where Funcionario = "Maria Santos";
+
+-- questao 11
+select * from ItensDeVenda iv
+inner join Produtos p on iv.IDDoProduto = p.ID
+where iv.IDDaVenda = 1; 
+
+-- questao 12
+-- desconsiderar, necessita de recursos mais avançados
+
+-- questao 13
